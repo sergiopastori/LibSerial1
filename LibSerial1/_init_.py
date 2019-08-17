04befbf8964069eaea6e42f03ae430f94e9a6303
sergiopastori <51001734+sergiopastori@users.noreply.github.com>
2019-08-17 15:13:43 -0300

    Add files via upload

---
+++ LibSerial1/_init_.py
@@ -0,0 +1,1 @@
+from LibSerial1.LibSerial1 import Serial, Uart
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'LibSerial1',       
+  packages = ['LibSerial1'],   
+  version = '0.1',      
+  license='MIT',       
+  description = 'Biblioteca LibSerial1 ',   
+  author = 'Sérgio Pastori',                  
+  author_email = 'sergiopastori@gmail.com',     
+  url = 'https://github.com/path/LibSerial1',  
+  download_url = 'https://github.com/path/LibSerial1/archive/0.1.tar.gz',    
+  keywords = ['Serial', 'Senai', 'ComPort'],  
+  install_requires=[           
+          'datetime',
+          'random',         
+      ],
+  classifiers=[
+    #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"   
+    'Development Status :: 3 - Alpha',   
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
